120ed6b25992492ac2bf0c316095173b6c6361c3
Ishika Saxena <isaxena@horizontal.com>
2023-10-28 18:14:55 +0530

    NOJIRA-000:banner-cmpt

--- src/components/authorable/General/Banner/Banner.mock-data.ts
+++ src/components/authorable/General/Banner/Banner.mock-data.ts
@@ -2,12 +2,39 @@ import { BannerProps } from './Banner';
 
 // Realistically this is going to have a lot more data from Sitecore or XM Cloud, but this is a good example mock data set.
 const defaultData: BannerProps = {
-  rendering: { componentName: 'Banner' },
-  params: {},
+  uid: '5bff2ebc-f89e-4e21-8ce7-e416c541f2bc',
+  componentName: 'Banner',
+  dataSource: '{13B449AF-DF82-4A95-9D01-BCE28211E493}',
+  // "params": {
+
+  // },
   fields: {
-    text: {
+    BannerImage: {
+      value: {
+        src: '/-/media/Default-Website/sc_logo.png?h=51&iar=0&w=204&hash=70185A6C46DD94DC14708A2FDC1539D0',
+        alt: '',
+        width: '204',
+        height: '51',
+      },
+    },
+    BannerHeading: {
+      value: 'Banner Heading',
+    },
+    BannerDescription: {
       value:
-        '<p>This is <em>richtext</em> content to show the <strong>simplest</strong> possible example of a implementaiton possible.</p>',
+        '\u003Cp\u003ELorem ipsum dolor sit amet consectetur adipisicing elit. Non quas saepe nesciunt praesentium illo minima, animi quos voluptatem odit debitis?\u003C/p\u003E',
+    },
+    BannerCTA: {
+      value: {
+        text: 'Test',
+        anchor: '',
+        linktype: 'internal',
+        class: '',
+        title: '',
+        querystring: '',
+        id: '{15CC85F3-AD65-4568-A0AA-5EAB1AFE3D09}',
+        href: '/test',
+      },
     },
   },
 };
