f5f3d36fc7f60a629d90fd224d579e7ed1649061
Ishika Saxena <isaxena@horizontal.com>
2023-10-29 11:01:54 +0530

    NOJIRA-000:theming

---
+++ src/lib/theming.ts
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+export type ThemeOptions = 'themeWhite' | 'themeBlack' | 'themeBlue';
+
+const themes = {
+  themeWhite: classNames('themeWhite', 'bg-theme-bg', 'text-theme-text'),
+  themeBlack: classNames('themeBlack', 'bg-theme-bg', 'text-theme-text'),
+  themeBlue: classNames('themeBlue', 'bg-theme-bg', 'text-theme-text'),
+};
+
+/**
+ * Return relevant classes for the selected theme, or return null
+ * if no additional theme classes need to be applied
+ * @param {ThemeOptions | 'inherit' | undefined} theme The theme option to get classes for
+ * @returns {TTailwindString | null} The relevant tailwind-classnames TArg
+ */
+
+const getThemeClasses = (theme: ThemeOptions | 'theme-inherit' | undefined): string | null => {
+  return typeof theme === 'undefined' || theme === 'theme-inherit' ? null : themes[theme];
+};
+
+export default getThemeClasses;
